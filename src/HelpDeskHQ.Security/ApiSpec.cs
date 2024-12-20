19af01ea0bac5e8e5e7b9fd45eb0f00c4aec9698
bestes83 <bestes83@gmail.com>
2024-12-20 17:56:03 -0600

    added login endpoint.

--- src/HelpDeskHQ.Security/ApiSpec.cs
+++ src/HelpDeskHQ.Security/ApiSpec.cs
@@ -1,11 +1,22 @@
-﻿namespace HelpDeskHQ.Security
+﻿using HelpDeskHQ.Core.Features.Security.Commands;
+using HelpDeskHQ.Core.Helpers;
+using HelpDeskHQ.Core.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskHQ.Security
 {
     public static class ApiSpec
     {
         public static IEndpointRouteBuilder MapV1Endpoints(this IEndpointRouteBuilder app)
         {
-            app.MapGet("/login", () => { });
+            app.MapPost("/login", Login);
             return app;
+        }
+
+        private static async Task<Response<AccountVm>> Login([AsParameters] IMediator mediator, [FromBody] LoginCommand loginCommand)
+        {
+            return await mediator.Send(loginCommand);
         }
     }
 }
--- src/HelpDeskHQ.Security/Program.cs
+++ src/HelpDeskHQ.Security/Program.cs
@@ -1,3 +1,5 @@
+using HelpDeskHQ.Core;
+using HelpDeskHQ.Persistence;
 using HelpDeskHQ.Security;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services
+    .RegisterDataLayer()
+    .RegisterServices();
 
 var app = builder.Build();
 
