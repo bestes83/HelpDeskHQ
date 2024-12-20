58d1b3b4f8a856239852fcb3386ff47fedcd60f9
bestes83 <bestes83@gmail.com>
2024-12-20 10:24:47 -0600

    moved solution to a src directory for better orgainization.

--- src/HelpDeskHQ.Core/Features/Security/Commands/LoginCommandHandler.cs
+++ src/HelpDeskHQ.Core/Features/Security/Commands/LoginCommandHandler.cs
@@ -4,6 +4,7 @@ namespace HelpDeskHQ.Core.Features.Security.Commands
 {
     public class LoginCommandHandler: IRequestHandler<LoginCommand>
     {
+
         public async Task Handle(LoginCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
