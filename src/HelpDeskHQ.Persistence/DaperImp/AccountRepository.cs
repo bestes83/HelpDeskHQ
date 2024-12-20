58797cf9d69547907228aefcf6390459339db637
bestes83 <bestes83@gmail.com>
2024-12-20 17:55:39 -0600

    added login method to account repository.

--- src/HelpDeskHQ.Persistence/DaperImp/AccountRepository.cs
+++ src/HelpDeskHQ.Persistence/DaperImp/AccountRepository.cs
@@ -1,7 +1,23 @@
-﻿namespace HelpDeskHQ.Persistence.DaperImp
-{
-    public class AccountRepository
-    {
+﻿using System.Runtime.InteropServices;
+using HelpDeskHQ.Core.Contracts;
+using HelpDeskHQ.Domain.Security;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
+
+namespace HelpDeskHQ.Persistence.DaperImp
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public Account? GetByUsernamePassword(string username, string password)
+        {
+            var sql = "SELECT AccountId, Username FROM Account WHERE Username=@Username && Password=@Password";
+
+            using (var connection = new SqlConnection(sql))
+            {
+                var account = connection.QuerySingleOrDefault<Account>(sql, new {Username = username, Password = password});
+                return account;
+            }
+        }
     }
 }
---
+++ src/HelpDeskHQ.Persistence/Global.cs
@@ -0,0 +1,16 @@
+﻿using HelpDeskHQ.Core.Contracts;
+using HelpDeskHQ.Persistence.DaperImp;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelpDeskHQ.Persistence
+{
+    public static class Global
+    {
+        public static IServiceCollection RegisterDataLayer(this IServiceCollection services)
+        {
+            services.AddScoped<IAccountRepository, AccountRepository>();
+
+            return services;
+        }
+    }
+}
